7c66f14d6e1ebdeac520894ca2384cf764968de0
Steven Rafferty <stevenrafferty2@protonmail.com>
2021-08-22 00:15:49 +0100

    Added create for meeting

--- src/pages/meetings/meetingsAdd/MeetingsAdd.tsx
+++ src/pages/meetings/meetingsAdd/MeetingsAdd.tsx
@@ -1,19 +1,41 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { ReactElement } from 'react';
-import { getStudentsNamesByClass } from '../../../services/students.service';
+import { useHistory } from 'react-router-dom';
+import { addMeeting } from '../../../services/meetings.service';
+import { getCurrencies } from '../../../services/selects.service';
+import { getStudentsNames } from '../../../services/students.service';
+import { getTutorsNames } from '../../../services/tutors.service';
 
 import { MeetingsAddView } from './MeetingsAddView';
 
 export const MeetingsAdd = (): ReactElement => {
+
+  const history = useHistory();
+
+  // State
   const [isLoadingState, setIsLoadingState] = useState(true);
   const [studentNamesState, setStudentNamesState] = useState([]);
   const [tutorNamesState, setTutorNamesState] = useState([]);
+  const [currenciesState, setCurrenciesState] = useState([]);
 
   useEffect(() => {
     (async () => {
       try {
-
+        const [tutorNames, studentsNames, currencies] = await Promise.all([
+          getTutorsNames(),
+          getStudentsNames(),
+          getCurrencies(),
+        ])
+        if (tutorNames.success) {
+          setTutorNamesState(tutorNames.payload.docs);
+        }
+        if (studentsNames.success) {
+          setStudentNamesState(studentsNames.payload.docs);
+        }
+        if (currencies.success) {
+          setCurrenciesState(currencies.payload);
+        }
         setIsLoadingState(false);
       } catch (error) {
         console.error('ERROR - MeetingsAdd.tsx - useEffect():', error);
@@ -23,9 +45,28 @@ export const MeetingsAdd = (): ReactElement => {
   }, []);
 
 
-  const onSubmit = (values) => {
-    console.log(values);
+  const onSubmit = async (values) => {
+    try {
+      if (
+        !values.periodStart ||
+        !values.periodEnd
+      ) return;
+      setIsLoadingState(true);
+      // Convert periodStart and periodEnd to timestamps
+      const newValues = {...values};
+      newValues.periodStart = newValues.periodStart.unix();
+      newValues.periodEnd = newValues.periodEnd.unix();
+  
+      const newMeeting = await addMeeting(newValues);
+      setIsLoadingState(false);
+      if (newMeeting.success) {
+        history.push('/meetings');
+      }
+    } catch (error) {
+      console.error('ERROR - MeetingsAdd.tsx - onSubmit():', error);
+      setIsLoadingState(false);
+    }
   };
 
-  return <MeetingsAddView onSubmit={onSubmit} classCodes={classCodesState} />;
+  return <MeetingsAddView onSubmit={onSubmit} tutorNames={tutorNamesState} studentNames={studentNamesState} currencies={currenciesState} />;
 };
--- src/pages/meetings/meetingsAdd/MeetingsAddView.tsx
+++ src/pages/meetings/meetingsAdd/MeetingsAddView.tsx
@@ -2,10 +2,13 @@ import { Field, Form, Formik } from "formik";
 import moment from "moment";
 import { ReactElement } from "react";
 import { Link } from "react-router-dom";
+import Datetime from 'react-datetime';
 
 export const MeetingsAddView = ({
   onSubmit,
-  classCodes,
+  tutorNames,
+  studentNames,
+  currencies,
 }): ReactElement => {
   return (
     <div>
@@ -23,13 +26,50 @@ export const MeetingsAddView = ({
         }}
         onSubmit={onSubmit}
       >
-        {({ values }) => (
-          <Form>
+        {({ values, setFieldValue }) => (
+          <Form
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
             <label>
-              Location:
+              Tutor:
               <Field name="tutorId" as="select">
                 <option value='None selected'>None selected</option>
-                {classCodes.map(c => <option value={c._id} key={c._id}>{c.classCode}</option>)}
+                {tutorNames.map(t => <option value={t._id} key={t._id}>{t.firstName} {t.lastName}</option>)}
+              </Field>
+            </label>
+            <label>
+              Student:
+              <Field name="studentId" as="select">
+                <option value='None selected'>None selected</option>
+                {studentNames.map(s => <option value={s._id} key={s._id}>{s.firstName} {s.lastName}</option>)}
+              </Field>
+            </label>
+            <label>
+              Period Start:
+              <Field
+                name="periodStart"
+                render={() => (
+                  <Datetime onChange={(e) => setFieldValue('periodStart', e)} value={values.periodStart} />
+                )}
+              />
+            </label>
+            <label>
+              Period End:
+              <Field
+                name="periodEnd"
+                render={() => (
+                  <Datetime onChange={(e) => setFieldValue('periodEnd', e)} value={values.periodEnd} />
+                )}
+              />
+            </label>
+            <label>
+              Currency:
+              <Field name="currency" as="select">
+                <option value='None selected'>None selected</option>
+                {currencies.map(c => <option value={c} key={c}>{c}</option>)}
               </Field>
             </label>
             <button type="submit">Submit</button>
